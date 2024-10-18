3532162772386d6ab2691f11704eed503b1cec94
Gabriel Pires <gabrielpires2991@gmail.com>
2024-10-18 13:10:01 -0300

    fix

--- persistence/jpa/src/main/java/com/groupseven/hunthub/persistence/jpa/UserJpa.java
+++ persistence/jpa/src/main/java/com/groupseven/hunthub/persistence/jpa/UserJpa.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.git
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 import com.groupseven.hunthub.domain.repository.UserRepository;
 import org.springframework.stereotype.Repository;
