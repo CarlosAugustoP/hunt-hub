cca4679029c98c07451b0cd6d006340ba9534fa5
CarlosAugustoP <capv2004@gmail.com>
2024-10-21 18:01:32 -0300

    fixing presentation module

--- presentation/backend/src/main/java/com/groupseven/User/UserController.java
+++ presentation/backend/src/main/java/com/groupseven/User/UserController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.groupseven.hunthub.persistence.jpa.repository.UserRepositoryImpl;
+import com.groupseven.hunthub.domain.repository.PoRepository;
+import com.groupseven.hunthub.domain.services.UserService;
 @RestController
 @RequestMapping("/user")
 /* Initial blueprint for a controller. This does not reflect how they will actually turn out. */
 public class UserController {
 
-    UserRepositoryImpl userRepository;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping("/login")
     public String login() {
