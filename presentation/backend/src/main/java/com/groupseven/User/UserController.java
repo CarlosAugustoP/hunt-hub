9d65f633b68cf9eaef55e661403e07e484ce987d
CarlosAugustoP <capv2004@gmail.com>
2024-10-21 17:53:44 -0300

    removing unnecessary maven command

--- presentation/backend/src/main/java/com/groupseven/User/UserController.java
+++ presentation/backend/src/main/java/com/groupseven/User/UserController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.groupseven.hunthub.persistence.jpa.repository.UserRepositoryImpl;
-
 @RestController
 @RequestMapping("/user")
+/* Initial blueprint for a controller. This does not reflect how they will actually turn out. */
 public class UserController {
+
+    UserRepositoryImpl userRepository;
 
     @RequestMapping("/login")
     public String login() {
