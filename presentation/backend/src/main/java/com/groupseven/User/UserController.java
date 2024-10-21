a2bb0803121bbc46e7378205fbb41bc69ca4ceb0
CarlosAugustoP <capv2004@gmail.com>
2024-10-21 17:33:33 -0300

    bug fix

--- presentation/backend/src/main/java/com/groupseven/User/UserController.java
+++ presentation/backend/src/main/java/com/groupseven/User/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.groupseven.hunthub.persistence.*;
+import com.groupseven.hunthub.persistence.jpa.repository.UserRepositoryImpl;
 
 @RestController
 @RequestMapping("/user")
