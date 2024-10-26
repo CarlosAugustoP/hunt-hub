6e0183131a284459f90938ec5e3e55099e15cdba
luismingati <luismingati@gmail.com>
2024-10-26 11:47:39 -0300

    fix: melhores tratamentos de erro no login

--- presentation/backend/src/main/java/com/groupseven/hunthub/presentation/backend/User/UserController.java
+++ presentation/backend/src/main/java/com/groupseven/hunthub/presentation/backend/User/UserController.java
@@ -1,14 +1,19 @@
 package com.groupseven.hunthub.presentation.backend.User;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import com.groupseven.hunthub.domain.services.TokenService;
 import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import com.groupseven.hunthub.domain.models.User;
 import com.groupseven.hunthub.domain.models.AuthenticationDTO;
@@ -37,10 +42,27 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO authDTO) {
-        var userNamePassword = new UsernamePasswordAuthenticationToken(authDTO.email(), authDTO.password());
-        var auth = this.authenticationManager.authenticate(userNamePassword);
-        var token = tokenService.generateToken((User) auth.getPrincipal());
-        return ResponseEntity.ok(token);
+        try {
+            var userNamePassword = new UsernamePasswordAuthenticationToken(authDTO.getEmail(), authDTO.getPassword());
+            var auth = this.authenticationManager.authenticate(userNamePassword);
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            Map<String, String> response = new HashMap<>();
+            response.put("error", "Credenciais inválidas");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 
     /* Esse método vai dizer que não tem id no retorno do JSON, mas não se preocupar, no banco aparece tudo certo. */
